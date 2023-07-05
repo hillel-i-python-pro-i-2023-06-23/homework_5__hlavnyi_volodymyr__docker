78102daff091bf0f661b40ac80eafb7795a5fc32
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-07-05 14:50:20 +0200

    add small annotation to procedures

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 from faker import Faker
 
 
+# Return string with - fake first name
 def get_random_name_4_greetings():
     faker = Faker()
     random_first_name = faker.first_name()
     return random_first_name
 
 
+# print Hi and same fake first Name
 def print_random_name_greetings():
     print(f"Hi {get_random_name_4_greetings()}!")
 
 
+# main procedure
+# wo any parameter
 def main():
     print_random_name_greetings()
