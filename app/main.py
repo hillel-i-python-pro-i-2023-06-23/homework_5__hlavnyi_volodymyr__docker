ec582493bd54a1c199df0e76dbd88e5b05e98c3b
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-07-05 12:06:02 +0200

    🎉 first commit all new files app.py working docker is not working

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from faker import Faker
+
+def get_random_name_4_greetings():
+    faker = Faker()
+    random_first_name = faker.first_name()
+    return random_first_name
+
+def print_random_name_greetings():
+    print(f"Hi {get_random_name_4_greetings()}!")
+
+def main():
+    print_random_name_greetings()
+
+main()
